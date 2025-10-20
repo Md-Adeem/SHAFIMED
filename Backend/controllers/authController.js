4c5a04cf8a6ea925055b3e7bbbdd018b4cac1454
frazhaidry <frazhaidry24@gmail.com>
2025-10-20 19:37:08 +0530

    2FA auth mail after signup

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -155,6 +155,14 @@ export const verifyOtpAndRegister = async (req, res) => {
 
     await newUser.save();
 
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
     // Send confirmation email
 await transporter.sendMail({
   from: `"ShaafiMed" <${process.env.EMAIL_USER}>`,
