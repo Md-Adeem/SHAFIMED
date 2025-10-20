d38f35eb66df73c93f956c9c7f46c7d36215be4c
Md Adeem <mdadeem2003@gmail.com>
2025-10-20 20:58:22 +0530

    Otp verification implemented

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,23 +1,23 @@
-import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-export default router;
-
-
 // import express from "express";
-// import { sendOtp, verifyOtpAndRegister, loginUser } from "../controllers/authController.js";
+// import { registerUser, loginUser } from "../controllers/authController.js";
 
-// const authRouter = express.Router();
+// const router = express.Router();
 
-// authRouter.post("/send-otp", sendOtp);
-// authRouter.post("/verify-otp", verifyOtpAndRegister);
-// authRouter.post("/login", loginUser);
+// router.post("/register", registerUser);
+// router.post("/login", loginUser);
 
-// export default authRouter;
+// export default router;
+
+
+import express from "express";
+import { sendOtp, verifyOtpAndRegister, loginUser } from "../controllers/authController.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/send-otp", sendOtp);
+authRouter.post("/verify-otp", verifyOtpAndRegister);
+authRouter.post("/login", loginUser);
+
+export default authRouter;
 
 
