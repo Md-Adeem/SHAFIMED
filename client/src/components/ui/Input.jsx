bf9f0fdee40f6982ba7b7233c97af8e808088b2e
inzamam-siddiqui <siddiquiinzmam319@gmail.com>
2025-11-20 22:28:25 +0530

    add dark mode support to Label, Input, and Textarea components

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -1,11 +1,17 @@
 function Label({ children }) {
-  return <label className="block text-sm font-medium text-gray-700">{children}</label>;
+  return <label className=" block text-sm font-semibold 
+        text-gray-800 
+        dark:text-gray-200">{children}</label>;
 }
 
 function Input({ className = "", ...props }) {
   return (
     <input
-      className={`mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-teal-500 ${className}`}
+      // className={`mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-teal-500 ${className}`}
+       className={`mt-1 w-full px-4 py-2 border rounded-lg
+    border-gray-300 text-gray-900
+    dark:bg-gray-800 dark:text-gray-100 dark:border-gray-700
+    focus:ring-2 focus:ring-cyan-500 ${className}`}
       {...props}
     />
   );
@@ -15,7 +21,11 @@ function Textarea({ className = "", rows = 4, ...props }) {
   return (
     <textarea
       rows={rows}
-      className={`mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-teal-500 ${className}`}
+      // className={`mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-teal-500 ${className}`}
+      className={`mt-1   w-full px-4 py-2 border rounded-lg
+    border-gray-300 text-gray-900
+    dark:bg-gray-800 dark:text-gray-100 dark:border-gray-700
+    focus:ring-2 focus:ring-cyan-500 ${className}`}
       {...props}
     />
   );
